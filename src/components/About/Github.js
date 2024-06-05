923978bae3255026186923cb4216dd97b7691671
Bhavisha262 <138797566+Bhavisha262@users.noreply.github.com>
2024-06-05 11:35:40 +0530

    po

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -9,7 +9,7 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="Bhavisha262"
+        username="Soumyajit4419"
         blockSize={15}
         blockMargin={5}
         color="#c084f5"
